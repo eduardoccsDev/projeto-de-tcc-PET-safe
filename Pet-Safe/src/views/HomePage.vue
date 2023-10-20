07315b8e74734ba92553e612120db3dae2b99ec5
Eduardo Castro <eduardoc.info@gmail.com>
2023-10-19 21:37:43 -0300

    [PET-SAFE] Added new content at home page

--- Pet-Safe/src/views/HomePage.vue
+++ Pet-Safe/src/views/HomePage.vue
@@ -34,50 +34,47 @@
       <ul class="nav nav-tabs" id="myTab" role="tablist">
         <li class="nav-item" role="presentation">
           <button
-            class="nav-link active"
-            id="home-tab"
+            class="nav-link"
+            id="ocultar-tab"
             data-bs-toggle="tab"
-            data-bs-target="#home"
+            data-bs-target="#ocultar"
             type="button"
             role="tab"
-            aria-controls="home"
-            :aria-selected="
-              userData && userData.residenciauser === 'Casa' ? 'true' : 'false'
-            "
+            aria-controls="ocultar"
+            aria-selected="false"
           >
-            Casa
+            <i class="fa-solid fa-minus"></i> Ocultar
           </button>
         </li>
         <li class="nav-item" role="presentation">
           <button
-            class="nav-link"
+            class="nav-link active"
             id="apt-tab"
             data-bs-toggle="tab"
             data-bs-target="#apt"
             type="button"
             role="tab"
             aria-controls="apt"
-            :aria-selected="
-              userData && userData.residenciauser === 'Apartemento' ? 'true' : 'false'
-            "
+            aria-selected="true"
           >
-            Apartamento
+            <i class="fa-solid fa-comment"></i> Dicas fundamentais
           </button>
         </li>
       </ul>
       <div class="tab-content" id="myTabContent">
         <div
           class="tab-pane fade show active"
-          id="home"
+          id="apt"
           role="tabpanel"
-          aria-labelledby="home-tab"
+          aria-labelledby="apt-tab"
         >
-          {{ userData.residenciauser }}
-        </div>
-        <div class="tab-pane fade" id="apt" role="tabpanel" aria-labelledby="apt-tab">
           <div class="cardsInfoContainer">
             <div class="info">
               <p class="info__title">Coloque redes de proteção nas janelas</p>
+              <figure class="info__figure">
+                <img src="../assets/images/janela-tela.webp" alt="janela-protecao" />
+                <figcaption>Fonte da imagem: iStock</figcaption>
+              </figure>
               <p class="info__description">
                 Ter uma tela de proteção é essencial para garantir a segurança dos
                 animais, seja qual for o seu andar. Seja um gato ou um cachorro, eles
@@ -87,6 +84,10 @@
             </div>
             <div class="info">
               <p class="info__title">Grades de proteção</p>
+              <figure class="info__figure">
+                <img src="../assets/images/porta-pet.webp" alt="porta-pet" />
+                <figcaption>Fonte da imagem: Compre reall</figcaption>
+              </figure>
               <p class="info__description">
                 Se você precisa delimitar espaço, invista nas grades de proteção. Ela vai
                 ser muito útil para evitar a entrada do seu animal em banheiros, por
@@ -97,6 +98,10 @@
             </div>
             <div class="info">
               <p class="info__title">Limpeza do ambiente</p>
+              <figure class="info__figure">
+                <img src="../assets/images/caixinha-limpa.jpg" alt="caixinha-limpa" />
+                <figcaption>Fonte da imagem: Adobe stock</figcaption>
+              </figure>
               <p class="info__description">
                 Organizar e limpar o local demanda tempo, mas vira uma tarefa ainda mais
                 importante quando se tem um animal em casa. Manter a higiene é essencial
@@ -107,6 +112,10 @@
             </div>
             <div class="info">
               <p class="info__title">Itens perigosos fora do alcance</p>
+              <figure class="info__figure">
+                <img src="../assets/images/pet-planta.webp" alt="planta" />
+                <figcaption>Fonte da imagem: Cobasi</figcaption>
+              </figure>
               <p class="info__description">
                 Além de janelas e outras áreas que podem oferecer algum risco aos animais,
                 é preciso ter cuidado com as plantas. Algumas espécies podem ter espinhos
@@ -115,6 +124,10 @@
             </div>
             <div class="info">
               <p class="info__title">Passeios são essenciais</p>
+              <figure class="info__figure">
+                <img src="../assets/images/passeio-com-caes.jpg" alt="passeio" />
+                <figcaption>Fonte da imagem: Dogscare</figcaption>
+              </figure>
               <p class="info__description">
                 E mesmo que você more em um local com muito espaço, levar o pet para
                 passear é importante para o bem-estar dele. E manter uma rotina de horário
@@ -124,6 +137,10 @@
             </div>
             <div class="info">
               <p class="info__title">Pet places</p>
+              <figure class="info__figure">
+                <img src="../assets/images/pet-place.jpg" alt="pet-place" />
+                <figcaption>Fonte da imagem: Auten</figcaption>
+              </figure>
               <p class="info__description">
                 Pet place nada mais é do que uma área de lazer completa e com outras
                 facilidades dentro do próprio condomínio para animais de estimação. Ou
@@ -184,27 +201,30 @@
 import FixedAlert from "../components/FixedAlert.vue";
 import HeroSection from "../components/HeroSection.vue";
 import HomeAccordionContent from "../components/HomeAccordionContent.vue";
-import axios from "axios";
-import { ref } from "vue";
-const userData = ref(null);
-const token = localStorage.getItem("token");
-
-if (token) {
-  axios
-    .get("http://localhost:3000/protegido", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .then((response) => {
-      userData.value = response.data.user;
-    })
-    .catch((error) => {
-      console.error("Erro ao acessar o endpoint protegido:", error);
-    });
-}
 </script>
 <style scoped lang="scss">
+#myTab {
+  margin-bottom: 20px;
+}
+
+.tab-pane {
+  background-color: #fff;
+  border-radius: 10px;
+  margin-bottom: 20px;
+}
+
+.nav-item {
+  button {
+    color: var(--primary);
+    background-color: var(--dark);
+    margin-left: 5px;
+
+    @media (max-width: 668px) {
+      font-size: 12px;
+    }
+  }
+}
+
 .cardsInfoContainer {
   display: flex;
   gap: 10px;
@@ -216,8 +236,25 @@ if (token) {
     border-radius: 10px;
     border: solid 2px var(--primary);
     padding: 10px;
-    width: 400px;
+    width: 405px;
     text-align: justify;
+
+    &__figure {
+      width: 100%;
+
+      img {
+        width: 100%;
+        object-fit: cover;
+        height: 200px;
+        border-radius: 10px;
+      }
+
+      figcaption {
+        text-align: center;
+        font-style: italic;
+        font-size: 12px;
+      }
+    }
 
     &__title {
       font-size: 18px;
